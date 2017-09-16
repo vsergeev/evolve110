a4ccb79b138c3e1000bf3cfe189a765dc7dc2467
Vanya A. Sergeev <v@sergeev.io>
2017-09-15 21:20:37 -0500

    contracts: switch to fixed number of cells (252)

--- contracts/Rule110.sol
+++ contracts/Rule110.sol
@@ -11,9 +11,10 @@ contract Rule110 {
         GameStateUpdated(initialCells);
     }
 
-    uint256 constant PAT1 = 0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe;
-    uint256 constant PAT2 = 0x9249249249249249249249249249249249249249249249249249249249249248;
-    uint256 constant MASK = 0x4924924924924924924924924924924924924924924924924924924924924924;
+    uint256 constant PAT1 = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
+    uint256 constant PAT2 = 0x4924924924924924924924924924924924924924924924924924924924924924;
+    uint256 constant MASK = 0x0492492492492492492492492492492492492492492492492492492492492492;
+    uint256 constant NUM_CELLS = 252;
 
     function evolve() {
         uint256 temp;
@@ -31,32 +32,26 @@ contract Rule110 {
         /* Find matches to pattern 111 */
         temp = state ^ PAT1;
         mat1 = (temp | (temp >> 1) | (temp << 1)) & MASK;
-        temp = ((state >> 1) | (state << 255)) ^ PAT1;
+        temp = ((state >> 1) | (state << (NUM_CELLS-1))) ^ PAT1;
         mat1 |= ((temp | (temp >> 1) | (temp << 1)) & MASK) << 1;
-        temp = ((state << 1) | (state >> 255)) ^ PAT1;
+        temp = ((state << 1) | (state >> (NUM_CELLS-1))) ^ PAT1;
         mat1 |= ((temp | (temp >> 1) | (temp << 1)) & MASK) >> 1;
-        temp = ((state << 2) | (state >> 254)) ^ PAT1;
-        mat1 |= ((temp | (temp >> 1) | (temp << 1)) & MASK) >> 2;
 
         /* Find matches to pattern 100 */
         temp = state ^ PAT2;
         mat2 = (temp | (temp >> 1) | (temp << 1)) & MASK;
-        temp = ((state >> 1) | (state << 255)) ^ PAT2;
+        temp = ((state >> 1) | (state << (NUM_CELLS-1))) ^ PAT2;
         mat2 |= ((temp | (temp >> 1) | (temp << 1)) & MASK) << 1;
-        temp = ((state << 1) | (state >> 255)) ^ PAT2;
+        temp = ((state << 1) | (state >> (NUM_CELLS-1))) ^ PAT2;
         mat2 |= ((temp | (temp >> 1) | (temp << 1)) & MASK) >> 1;
-        temp = ((state << 2) | (state >> 254)) ^ PAT2;
-        mat2 |= ((temp | (temp >> 1) | (temp << 1)) & MASK) >> 2;
 
         /* Find matches to pattern 000 */
         temp = state;
         mat3 = (temp | (temp >> 1) | (temp << 1)) & MASK;
-        temp = ((state >> 1) | (state << 255));
+        temp = ((state >> 1) | (state << (NUM_CELLS-1)));
         mat3 |= ((temp | (temp >> 1) | (temp << 1)) & MASK) << 1;
-        temp = ((state << 1) | (state >> 255));
+        temp = ((state << 1) | (state >> (NUM_CELLS-1)));
         mat3 |= ((temp | (temp >> 1) | (temp << 1)) & MASK) >> 1;
-        temp = ((state << 2) | (state >> 254));
-        mat3 |= ((temp | (temp >> 1) | (temp << 1)) & MASK) >> 2;
 
         /* Any match to patterns 111, 100, 000, will have a 0 bit in mat1,
          * mat2, or mat3, respectively in that cell position. Otherwise, that
--- test/rule110.js
+++ test/rule110.js
@@ -3,7 +3,7 @@ var Rule110Factory = artifacts.require("Rule110Factory");
 
 contract('Rule110', function(accounts) {
   it("should initialize state correctly", function () {
-    var initialCells = web3.toBigNumber('0x9ce6a5b47848254f5852feca4c7592ace15a64732879bed96374d2a87a6177ab');
+    var initialCells = web3.toBigNumber('0x788b997abd8e68126f0c93181afa5d2fd645a353f490e96af75760f2ac01032');
 
     return Rule110.new(initialCells).then(function (instance) {
       var receipt = web3.eth.getTransactionReceipt(instance.transactionHash);
@@ -15,26 +15,26 @@ contract('Rule110', function(accounts) {
     })
   });
   it("should evolve correctly", function () {
-    var initialCells = web3.toBigNumber("0x26f89bef8ee26f89be26837c765be26f89be26f89be26f8e04df137c4df137c7");
+    var initialCells = web3.toBigNumber("0x788b997abd8e68126f0c93181afa5d2fd645a353f490e96af75760f2ac01032");
 
     var testVectors = [
-        web3.toBigNumber('0x6f89be389ba6f89be26f87c4defe26f89be26f89be26f89a0df137c4df137c4d'),
-        web3.toBigNumber('0xf89be269beef89be26f88c4df3826f89be26f89be26f89be1f137c4df137c4df'),
-        web3.toBigNumber('0x09be26fbe3b89be26f899cdf1686f89be26f89be26f89be23137c4df137c4df0'),
-        web3.toBigNumber('0x1be26f8e26e9be26f89bb5f13f8f89be26f89be26f89be26737c4df137c4df10'),
-        web3.toBigNumber('0x3e26f89a6fbbe26f89beff1360989be26f89be26f89be26ed7c4df137c4df130'),
-        web3.toBigNumber('0x626f89bef8ee26f89be38137e1b9be26f89be26f89be26fbfc4df137c4df1370'),
-        web3.toBigNumber('0xe6f89be389ba6f89be26837c23ebe26f89be26f89be26f8e04df137c4df137d0'),
-        web3.toBigNumber('0xaf89be269beef89be26f87c4663e26f89be26f89be26f89a0df137c4df137c71'),
-        web3.toBigNumber('0xf89be26fbe3b89be26f88c4cee626f89be26f89be26f89be1f137c4df137c4d3'),
-        web3.toBigNumber('0x09be26f8e26e9be26f899cddbae6f89be26f89be26f89be23137c4df137c4df6'),
+      web3.toBigNumber('0xc99ebbcfe79af836f91db7383f8ef7787ecfe7f61db1bbff9dfde197fc03076'),
+      web3.toBigNumber('0xdbb3ee582cbf887f8b37fd68609b9dc8c3d82c1e37f3ee00b70723bc04070df'),
+      web3.toBigNumber('0x7ef63af87de098c09f7c07f8e1beb759c6787c327c163a01fd0d66e40c0d1f0'),
+      web3.toBigNumber('0xc39e6f88c721b9c1b1c40c09a3e3fdfb4ec8c476c43e6e03071fefac1c1f310'),
+      web3.toBigNumber('0xc6b2f899cd63eb43f34c1c1be626070fdbd9ccdfcc62fa070d3038fc3431731'),
+      web3.toBigNumber('0x4ff789bb5fe63fc617dc343e2e6e0d187e7b5df05ce78e0d1f7069847c73d73'),
+      web3.toBigNumber('0xd81c9beff02e604e3c747c627afa1f38c2cff710f5ac9a1f31d0fb8cc4d67d7'),
+      web3.toBigNumber('0x7835be38107ae0da64dcc4e6cf8e3169c7d81d319ffdbe3173718e9dcdfec7c'),
+      web3.toBigNumber('0xc87fe26830cfa1feedf5cdafd89a73fb4c783773b007e273d7d39bb75f03cc4'),
+      web3.toBigNumber('0xd8c026f871d8e303bf1f5ff879bed60fdcc87dd6f00c26d67c76befdf1065cd'),
     ]
 
     return Rule110.new(initialCells).then(function (instance) {
       return testVectors.reduce(function (acc, vector) {
         return acc.then(function () {
           return instance.evolve().then(function (result) {
-            assert(result.receipt.gasUsed < 50000, "Excessive gas used");
+            assert(result.receipt.gasUsed < 30000, "Excessive gas used");
             assert.equal(result.logs.length, 1, "Invalid number of events produced");
             assert.equal(result.logs[0].event, "GameStateUpdated", "Invalid event produced");
             assert(web3.toBigNumber(result.logs[0].args.cells.toString()).equals(vector), "Invalid state");
@@ -52,12 +52,12 @@ contract('Rule110', function(accounts) {
 
 contract('Rule110Factory', function(accounts) {
   it("should deploy correctly", function () {
-    var initialCells = web3.toBigNumber('0x9ce6a5b47848254f5852feca4c7592ace15a64732879bed96374d2a87a6177ab');
+    var initialCells = web3.toBigNumber('0x788b997abd8e68126f0c93181afa5d2fd645a353f490e96af75760f2ac01032');
     var description = "foobar";
 
     return Rule110Factory.deployed().then(function (instance) {
       return instance.newRule110(initialCells, description).then(function (result) {
-        assert(result.receipt.gasUsed < 300000, "Excessive gas used");
+        assert(result.receipt.gasUsed < 200000, "Excessive gas used");
         assert.equal(result.logs.length, 1, "Invalid number of events produced");
         assert.equal(result.logs[0].event, "GameCreated", "Invalid event produced");
         assert.equal(web3.toUtf8(result.logs[0].args.description), description, "Invalid description");
