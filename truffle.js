21978c807d99352b0d7e5a276e872317cc101e99
Vanya A. Sergeev <v@sergeev.io>
2017-09-18 23:45:22 -0500

    config: add testnet networks for deployment

--- truffle.js
+++ truffle.js
@@ -1,3 +1,24 @@
+var provider = null;
+
+if (process.env.DEPLOY) {
+  var HDWalletProvider = require("truffle-hdwallet-provider");
+
+  var mnemonic = process.env.DEPLOY_WALLET_MNEMONIC;
+  var providerUrl = process.env.DEPLOY_PROVIDER_URL;
+
+  if (mnemonic == undefined) {
+    console.error("DEPLOY_WALLET_MNEMONIC not specified.");
+    process.exit(1);
+  }
+
+  if (providerUrl == undefined) {
+    console.error("DEPLOY_PROVIDER_URL not specified.");
+    process.exit(1);
+  }
+
+  provider = new HDWalletProvider(mnemonic, providerUrl);
+}
+
 module.exports = {
   networks: {
     development: {
@@ -9,6 +30,24 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "1234",
-    }
+    },
+    ropsten: {
+      provider: provider,
+      gas: "700000",
+      gasPrice: "20000000000",
+      network_id: "3",
+    },
+    kovan: {
+      provider: provider,
+      gas: "700000",
+      gasPrice: "20000000000",
+      network_id: "42",
+    },
+    rinkeby: {
+      provider: provider,
+      gas: "700000",
+      gasPrice: "20000000000",
+      network_id: "4",
+    },
   }
 };
