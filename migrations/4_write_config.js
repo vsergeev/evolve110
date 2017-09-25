707226fdf20989e62c7ba5d2b4f904c46cfc20d9
Vanya A. Sergeev <v@sergeev.io>
2017-09-24 20:38:37 -0500

    migrations: update factory block numbers in write config

--- migrations/4_write_config.js
+++ migrations/4_write_config.js
@@ -18,31 +18,31 @@ module.exports = function(deployer, network) {
     networks: {
       1: {
         factoryAddress: null,
-        factoryBlockNumber: 0,
+        factoryBlockNumber: "4315504",
         defaultGasPrice: "5",
         tipAddress: "0x36D87730191d02Cf0E4c9f4c1d686D65126793bd",
       },
       3: {
         factoryAddress: null,
-        factoryBlockNumber: 0,
+        factoryBlockNumber: "1740503",
         defaultGasPrice: "20",
         tipAddress: "0x77609A77DF4Bb5c8464ee1c84B17F934297D35F5",
       },
       4: {
         factoryAddress: null,
-        factoryBlockNumber: 0,
+        factoryBlockNumber: "977751",
         defaultGasPrice: "20",
         tipAddress: "0x77609A77DF4Bb5c8464ee1c84B17F934297D35F5",
       },
       42: {
         factoryAddress: null,
-        factoryBlockNumber: 0,
+        factoryBlockNumber: "3992367",
         defaultGasPrice: "20",
         tipAddress: "0x77609A77DF4Bb5c8464ee1c84B17F934297D35F5",
       },
       1234: {
         factoryAddress: null,
-        factoryBlockNumber: 0,
+        factoryBlockNumber: "0",
         defaultGasPrice: "4",
         tipAddress: "0xecf0744f2f71a16b831125d6cffafd6095617adf",
       },
