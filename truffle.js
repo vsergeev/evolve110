c6a5019c269b38fa11daae8b85bb411650dcc54f
Vanya A. Sergeev <v@sergeev.io>
2017-09-27 02:01:02 -0500

    config: default to local node for deployment

--- truffle.js
+++ truffle.js
@@ -22,38 +22,46 @@ if (process.env.DEPLOY) {
 module.exports = {
   networks: {
     development: {
+      network_id: "*",
       host: "localhost",
       port: 8545,
-      network_id: "*",
     },
     local: {
+      network_id: "1234",
       host: "localhost",
       port: 8545,
-      network_id: "1234",
     },
     mainnet: {
+      network_id: "1",
       provider: provider,
+      host: "localhost",
+      port: 8545,
       gas: "700000",
       gasPrice: "5000000000",
-      network_id: "1",
     },
     ropsten: {
+      network_id: "3",
       provider: provider,
+      host: "localhost",
+      port: 8545,
       gas: "700000",
       gasPrice: "20000000000",
-      network_id: "3",
     },
     kovan: {
+      network_id: "42",
       provider: provider,
+      host: "localhost",
+      port: 8545,
       gas: "700000",
       gasPrice: "20000000000",
-      network_id: "42",
     },
     rinkeby: {
+      network_id: "4",
       provider: provider,
+      host: "localhost",
+      port: 8545,
       gas: "700000",
       gasPrice: "20000000000",
-      network_id: "4",
     },
   }
 };
