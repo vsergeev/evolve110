4a9a00ea55dfe5707799d86f4d4ba3cfbce4e621
Vanya A. Sergeev <v@sergeev.io>
2020-03-24 22:40:43 -0500

    start: replace testrpc with ganache

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# Start testrpc
-testrpc -i 1234 --account="0x9933f4c423eebf2ecd1320150a901c1727991a93ce9619549352c8b4377aef26,100000000000000000000" --account="0x5e33dcd69f9501cd02432ac5c9fab48e1a53357c3b7e97e59faf2c088a94b8df,1000000000000000000" -u 0 &
-TESTRPC_PID=$!
+# Start ganache
+ganache-cli -i 1234 --account="0x9933f4c423eebf2ecd1320150a901c1727991a93ce9619549352c8b4377aef26,100000000000000000000" --account="0x5e33dcd69f9501cd02432ac5c9fab48e1a53357c3b7e97e59faf2c088a94b8df,1000000000000000000" -u 0 &
+GANACHE_PID=$!
 
 sleep 1
 
@@ -10,8 +10,8 @@ sleep 1
 truffle migrate --network local
 
 function cleanup() {
-    echo "Stopping testrpc..."
-    kill $TESTRPC_PID
+    echo "Stopping ganache..."
+    kill $GANACHE_PID
 }
 trap cleanup 2
 
